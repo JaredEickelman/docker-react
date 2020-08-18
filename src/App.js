2f01fb6cb84bc4636993f04be30a5a413444985b
jeickelman-isp <jeickelman@istreamplanet.com>
2020-08-18 12:34:48 -0500

    check

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          for reals
+          live updating!!!
         </p>
         <a
           className="App-link"
